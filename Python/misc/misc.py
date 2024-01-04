b1c633aa7df42db11753c40080c90391a10236f5
joehinmoro <joehinmoro@gmail.com>
2024-01-04 14:03:10 +0100

    240104

---
+++ Python/misc/misc.py
@@ -0,0 +1,31 @@
+# epl = {
+#     "man_city": 98,
+#     "villa": 92,
+#     "arsenal": 90
+# }
+
+# # for c, p in epl.items():
+# #     print(c, p)
+
+# print(list(epl.values()))
+
+# dict_1 = {"foo":"bar", "egg":"nog", "2": "tuu"}
+# dict_2 = {"foo":"baz", 2:"two"}
+
+# # dict_1.update(dict_2)
+
+# print({**dict_2, **dict_1})
+
+count = int(input("How many numbers?\n> "))
+nums = []
+
+for idx in range(count):
+    nums.append(float(input(f"enter num {idx + 1} of {count}\n> ")))
+
+def average(*args):
+    total = 0
+    for arg in args:
+        total += arg
+    return round(total / len(args), 2)
+
+print(average(*nums))
