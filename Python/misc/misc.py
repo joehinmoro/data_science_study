6ddad98bf9c11e3bfd402a8d7b09ebd50dd8d93e
joehinmoro <joehinmoro@gmail.com>
2024-02-28 15:22:50 +0100

    240228

--- Python/misc/misc.py
+++ Python/misc/misc.py
@@ -35,15 +35,37 @@
 
 #########################################################################
 
-def student_score(first, last, *score):
-    print(f"{first.capitalize()} {last.capitalize()} scored {score}")
+# def student_score(first, last, *score):
+#     print(f"{first.capitalize()} {last.capitalize()} scored {score}")
 
-# student_score(input("first\t"), input("last\t"), *[input("score 1\t"),input("score 2\t"),input("score 3\t")])
-student_score("Mary")
+# # student_score(input("first\t"), input("last\t"), *[input("score 1\t"),input("score 2\t"),input("score 3\t")])
+# student_score("Mary")
 
+#########################################################################
 
+def age(num, **callee):
+    
+    print(f"{num} age records:")
+    for person, age in callee:
+        print(f"{person} is {age} years old")
 
-
+age =  input()
 
 
 #########################################################################
+
+ocean_temperatures = {
+    'Pacific': [25, 25.3, 25.6, 25.2, 25.5, 25.6],
+    'Atlantic': [23.4, 23.8, 23.7, 29.9, 24.0, 29.1],  # Exclude > 29
+    'Indian': [26.5, 26.7, 27.0, -29, -55, -99, -99]    # Exclude < 0
+}
+
+{key: (min([temp for temp in val if 0 < temp < 29]), max([temp for temp in val if 0 < temp < 29])) for key, val in ocean_temperatures.items() }
+
+[num ** 2 for num in [num for num in range(21) if not num & 1] if not num % 4 if num]
+
+
+[l.upper() for l in "hello"]
+
+[i for i in range(10) if not i & 1 if i]
+
